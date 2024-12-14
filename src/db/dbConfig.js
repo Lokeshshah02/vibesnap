9e10b28a06c50851d264e4c454bd3365cdb711c3
Lokesh Shah <118357743+Lokeshshah02@users.noreply.github.com>
2024-12-14 20:37:49 +0530

    .env configured

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ymlfemualzqpmtxlqtol.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltbGZlbXVhbHpxcG10eGxxdG9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4OTY5MzYsImV4cCI6MjA0OTQ3MjkzNn0.UOvv0k8V7tKnkhQFlJiAnC9se5k1uyW7uc-uRuSsLZs";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 export default supabase;
